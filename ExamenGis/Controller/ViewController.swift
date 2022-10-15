736facc7a1a9b11355e0a77ff25f171875606d7b
Semjase <gisela.semjase@gmail.com>
2022-10-15 15:58:54 -0500

    UPDATE:  Se conectan datos con celdas de canciones y álbumes

--- ExamenGis/Controller/ViewController.swift
+++ ExamenGis/Controller/ViewController.swift
@@ -12,11 +12,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblArtist: UILabel!
     @IBOutlet weak var lblAlbum: UILabel!
     @IBOutlet weak var lblYear: UILabel!
-    @IBOutlet weak var tblSongs: UITableView!
+    @IBOutlet weak var tblSongs: UITableView!{
+        didSet{
+            self.tblSongs.delegate = self
+            self.tblSongs.dataSource = self
+            self.tblSongs.register(UINib(nibName: "SongsTableViewCell", bundle: .main), forCellReuseIdentifier: "SongsTableViewCell")
+        }
+    }
     @IBOutlet weak var tblAlbums: UITableView!{
         didSet {
             self.tblAlbums.delegate = self
             self.tblAlbums.dataSource = self
+            self.tblAlbums.register(UINib(nibName: "MusicTableViewCell", bundle: .main), forCellReuseIdentifier: "MusicTableViewCell")
         }
     }
     
@@ -26,13 +33,10 @@ class ViewController: UIViewController {
     let songs: [String] = ["Cancion 1","Cancion 2","Cancion 3","Cancion 4", "Cancion 5", "Cancion 6", "Cancion 7", "Cancion 8","Cancion 9","Cancion 10"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.tblAlbums.register(UINib(nibName: "MusicTableViewCell", bundle: .main), forCellReuseIdentifier: "MusicTableViewCell")
-    
-    
+        
     }
-
-
+    
+    
 }
 extension ViewController:UITableViewDelegate{}
 extension ViewController:UITableViewDataSource{
@@ -41,10 +45,14 @@ extension ViewController:UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MusicTableViewCell", for: indexPath) as? MusicTableViewCell ?? MusicTableViewCell()
-  
-        
-        return cell 
+        if tableView.tag == 2{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MusicTableViewCell", for: indexPath) as? MusicTableViewCell ?? MusicTableViewCell()
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SongsTableViewCell", for: indexPath) as? SongsTableViewCell ?? SongsTableViewCell()
+            cell.setCell(withNoSong: "\(indexPath.row + 1)", andNameSong: songs[indexPath.row])
+            return cell
+        }
     }
 }
 
